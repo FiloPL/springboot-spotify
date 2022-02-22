ba34f17e384ae0a566024ccf0b5f619987042108
FiloPL <tzegarlicki@gmail.com>
2022-02-22 18:42:28 +0100

    Initial commit

---
+++ src/test/java/com/filopl/springbootspotify/SpringbootSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filopl.springbootspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
